bf606116a7b0dd20bda16b63c6800c9393f70fed
Mikaio <mikaioharison038@gmail.com>
2022-08-26 13:58:26 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoDeRestaurante/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoDeRestaurante.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
