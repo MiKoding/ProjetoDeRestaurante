8fd819a2980744438e8621342ae4f15c03c16088
MiKoding <mikaioharison038@gmail.com>
2022-10-07 16:32:05 -0300

    strings de conexao inseridas no projeto.

---
+++ ProjetoDeRestaurante/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoDeRestaurante.Models;
+
+namespace ProjetoDeRestaurante.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+    }
+}
