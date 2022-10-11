16ffb2d6859e83022b34f0ef363f31322c218814
Mikaio <mikaioharison038@gmail.com>
2022-10-11 16:28:42 -0300

    DataAnnotations implementado na classe pedidos

--- ProjetoDeRestaurante/Models/Pedido.cs
+++ ProjetoDeRestaurante/Models/Pedido.cs
@@ -1,9 +1,22 @@
-﻿namespace ProjetoDeRestaurante.Models
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoDeRestaurante.Models
 {
+
+    [Table("Pedido")]
     public class Pedido
     {
-        public int Id { get; set; } 
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="O nome do pedido deve ser informado")]
+        [Display(Name = "Nome do Pedido")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "A descrição do pedido deve ser informado")]
+        [Display (Name = "Descrição do pedido")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição não pode exceder {1} caracteres")]
         public string DescricaoCurta { get; set; }
         public string DescricaoDetalhada { get; set; }
         public decimal Preco { get; set; }  
@@ -12,7 +25,9 @@
         public bool IsPedidoPreferido { get; set; }
         public bool EmEstoque { get; set; } 
 
-        public int CategoriaId { get; set; }    
+        public int CategoriaId { get; set; }
+        [NotMapped]
+        public DateTime DataDeCadastro { get; set; }
         public virtual Categoria Categoria { get; set; }
 
 
