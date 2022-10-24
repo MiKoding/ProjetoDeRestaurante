09fbad957abd3b3f78568c73ef6115b95675710a
MiKoding <mikaioharison038@gmail.com>
2022-10-24 19:09:15 -0300

    registrando o serviço dos repositorios

--- ProjetoDeRestaurante/Program.cs
+++ ProjetoDeRestaurante/Program.cs
@@ -20,12 +20,16 @@
 
 using ProjetoDeRestaurante.Context;
 using Microsoft.EntityFrameworkCore;
+using ProjetoDeRestaurante.Repositories.Interface;
+using ProjetoDeRestaurante.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddTransient<IPedidoRepository, PedidoRepository>();
+builder.Services.AddTransient<ICategoriaRepository, CategoriaRepository>();
 
 var app = builder.Build();
 
