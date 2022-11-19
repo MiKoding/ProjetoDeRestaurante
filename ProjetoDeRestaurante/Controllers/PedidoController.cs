6f1171eb76a84b454979ea22161a3a95139b359b
MiKoding <mikaioharison038@gmail.com>
2022-11-19 20:47:13 -0300

    pedidocontroller atualizado com novos dados na list

---
+++ ProjetoDeRestaurante/Controllers/PedidoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoDeRestaurante.Repositories.Interface;
+using ProjetoDeRestaurante.ViewModels;
+
+namespace ProjetoDeRestaurante.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+
+        public PedidoController(IPedidoRepository pedidoRepository)
+        {
+            _pedidoRepository = pedidoRepository;
+        }
+
+        public IActionResult List()
+        {
+            ViewData["Titulo"] = "Todos os pedidos";
+            ViewData["Data"] = DateTime.Now;
+            var pedidos = _pedidoRepository.Pedidos;
+            var TotalLanches = pedidos.Count();
+            ViewBag.Total = "Total de Lanches:";
+            ViewBag.TotalLanches = TotalLanches;
+            //return View(pedidos);
+            var pedidoListViewModel = new PedidoListViewModel();
+            pedidoListViewModel.Pedido = _pedidoRepository.Pedidos;
+            pedidoListViewModel.CategoriaAtual = "Categoria Atual";
+            return View(pedidoListViewModel);
+        }
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+    }
+}
