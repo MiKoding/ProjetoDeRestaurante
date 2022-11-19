f42f80fa2c32b004395e954a0d71e19715df430e
MiKoding <mikaioharison038@gmail.com>
2022-11-19 20:47:42 -0300

    variaveis aplicadas para o migration em carrinhocompraitem

---
+++ ProjetoDeRestaurante/Models/CarrinhoCompraItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace ProjetoDeRestaurante.Models
+{
+    public class CarrinhoCompraItem
+    {
+        public int Id { get; set; } 
+        public Pedido Pedido { get; set; }
+        public int Quantidade { get; set; }
+        [StringLength(200)]
+        public string CarrinhoCompraId { get; set; }
+    }
+}
