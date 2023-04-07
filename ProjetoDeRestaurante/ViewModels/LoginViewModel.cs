57615caf2db1ddfc746cbbccadc1b7e7cf6edcc4
MiKoding <mikaioharison038@gmail.com>
2023-04-07 17:30:58 -0300

    update: login and register implemented

---
+++ ProjetoDeRestaurante/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoDeRestaurante.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o usuário")]
+        [Display(Name ="Usuário")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Informe a senha")]
+        [DataType(DataType.Password)]
+        [Display(Name ="Senha")]
+        public string Pàssword { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
