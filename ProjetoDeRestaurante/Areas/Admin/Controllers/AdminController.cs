6a68fe2a681a7b4560a4c17a9993bf2243bc8ef2
MiKoding <mikaioharison038@gmail.com>
2023-06-01 18:26:33 -0300

    update: implemented admin area

---
+++ ProjetoDeRestaurante/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoDeRestaurante.Areas.Admin.Controllers
+{
+    [Authorize]
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
